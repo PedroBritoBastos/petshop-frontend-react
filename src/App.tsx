2e7309152ea213305bc2c71abbdfc1269e9a0bc1
pedro <pedrobritobastos234@gmail.com>
2026-06-17 17:51:58 -0300

    feature: cria rota /client

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import ClientPage from "./modules/client/pages/ClientPage";
+
 function App() {
   return (
-    <>
-      <h1 className='bg-red-100'>Hello World</h1>
-    </>
+    <Routes>
+      <Route path='/client' element={<ClientPage />} />
+    </Routes>
   )
 }
 
 export default App
+
