b5542f419a4c4048b66a24458d733c3077ef518b
pedro <pedrobritobastos234@gmail.com>
2026-06-17 18:00:40 -0300

    feature: cria navbar inicial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,11 @@
 export function Navbar() {
   return (
-    <nav></nav>
+    <nav className="w-full border-accent border-b-1 p-4 flex flex-col md:flex-row md:justify-between gap-5">
+      <div className="flex items-center justify-between gap-5">
+        <div className="flex items-center gap-2">
+          <h1 className="text-xl text-accent-foreground font-bold">Petshop</h1>
+        </div>
+      </div>
+    </nav>
   )
 }
