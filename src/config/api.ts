545635ff84692db156c68abf172864e68e7e3f7f
pedro <pedrobritobastos234@gmail.com>
2026-06-30 15:33:22 -0300

    feat(auth): cria metodo login em authService

--- src/config/api.ts
+++
@@ -1,6 +0,0 @@
-export const apiConfig: RequestInit = {
-  credentials: "include",
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
--- src/services/auth/authService.ts
+++ src/services/auth/authService.ts
@@ -1,1 +1,15 @@
 const API = import.meta.env.VITE_API_URL;
+
+export async function login(formData: FormData) {
+  const response = await fetch(`${API}/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    body: formData,
+  });
+
+  if (!response.ok) {
+    throw new Error("Falha ao realizar login.");
+  }
+
+  return response.json();
+}
