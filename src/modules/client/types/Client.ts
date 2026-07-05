7c8359a5d4334879914bdf21d57b599bcdfcc207
pedro <pedrobritobastos234@gmail.com>
2026-07-05 08:58:36 -0300

    feat(client): cria tipo Client

---
+++ src/modules/client/types/Client.ts
@@ -0,0 +1,11 @@
+export type Client = {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  cpf: string;
+  imageUrl?: string;
+  role: string;
+  createdAt: string;
+  updatedAt: string;
+};
