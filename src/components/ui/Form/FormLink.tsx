114c5d905c300e075fc2972de97412290e8b4f9b
pedro <pedrobritobastos234@gmail.com>
2026-07-06 17:57:53 -0300

    fix(client): corrige bug do form de registro

--- src/components/ui/Form/FormLink.tsx
+++ src/components/ui/Form/FormLink.tsx
@@ -9,12 +9,18 @@ interface Props {
 export function FormLink({ title, href, linkTitle }: Props) {
   const navigate = useNavigate();
 
+  function handleNavigate(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    navigate(`${href}`)
+  }
+
   return (
     <div className="text-lg text-card-foreground mt-2">
       {title}
       <button
         className="ml-2 text-card-foreground/60 hover:text-card-foreground/40 cursor-pointer"
-        onClick={() => navigate(`${href}`)}
+        onClick={handleNavigate}
       >
         {linkTitle}
       </button>
