6081f81c9f1ff927b7df6f5ccf6c03d0453aabc2
pedro <pedrobritobastos234@gmail.com>
2026-07-20 14:54:57 -0300

    fix(loading): corrige props do context

--- src/context/LoadingContext.ts
+++ src/context/LoadingContext.ts
@@ -2,6 +2,8 @@ import { createContext } from "react";
 
 type LoadingContextProps = {
   loading: boolean;
+  startLoading: () => void;
+  fisnishLoading: () => void;
 };
 
 export const LoadingContext = createContext<LoadingContextProps>({} as LoadingContextProps);
