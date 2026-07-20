954ab04290855b9f798f937f8c08e255cad1874e
pedro <pedrobritobastos234@gmail.com>
2026-07-20 14:59:00 -0300

    feat(loading): adiciona o provider no main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContextProvider.tsx'
 import { ErrorMessageContextProvider } from './context/ErrorMessageContextProvider.tsx'
+import { LoadingContextProvider } from './context/LoadingContextProvider.tsx'
 import './index.css'
 import App from './App.tsx'
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <AuthContextProvider>
         <ErrorMessageContextProvider>
-          <App />
+          <LoadingContextProvider>
+            <App />
+          </LoadingContextProvider>
         </ErrorMessageContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
